f562524fadd5699c46039ed9ab035867f58f8c18
Rob Berry <rob@luvhate.us>
2010-12-15 13:46:54 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "beget",
+    version = "0.1",
+    url = 'http://github.com/rob-b/Beget',
+    license = 'BSD',
+    description = "Django project generator",
+    author = 'rob-b',
+    packages = ['beget'],
+    scripts=['bin/beget'],
+    install_requires = ['setuptools'],
+    # package_dir = {'': '.'},
+)
