1cd6d447312210f63bf864e78957787c18afec85
Rob Berry <rob@luvhate.us>
2011-08-22 02:10:18 -0700

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
 from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 
 setup(
@@ -7,11 +10,18 @@ setup(
     url='http://github.com/rob-b/Beget',
     license='BSD',
     description="Django project generator",
+    long_description=README,
     author='Rob Berry',
+    author_email='',
     packages=['beget'],
     scripts=['bin/beget'],
     package_data={'beget': ['project_template/setup.py',
                             'project_template/project/*py']},
     tests_require=['fudge'],
     zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )
