ab601594a78701eec1365a580d87fd16b1d7b876
Rob Berry <rob@luvhate.us>
2012-11-28 09:29:07 +0000

    Add jenkins build script

---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -d ".env" ]; then
+ echo "**> virtualenv exists"
+else
+ echo "**> creating virtualenv"
+ virtualenv .env
+ mkdir -p $HOME/pip-cache
+ .env/bin/pip install --download-cache $HOME/pip-cache nose==1.1.2 coverage pylint
+fi
+
+. .env/bin/activate
+PYTHONPATH=. nosetests --with-xunit --with-coverage --cover-package=beget --cover-inclusive beget/tests.py
+coverage xml
+pylint -f parseable -d I0011,R0801 beget | tee pylint.out
+
